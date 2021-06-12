d38432535eb239b281b035fb121db7be33b11a25
Omer Shubi <omer.shubi@gmail.com>
2021-06-12 19:12:59 +0300

    Working on permuations of S_sigma (try with permutations on df.index

--- ex3_206348187_312236219.py
+++ ex3_206348187_312236219.py
@@ -7,20 +7,44 @@ CARS = ['bmw', 'vw', 'ford', 'kia', 'ferrari']
 YEARS = [2012, 2013, 2014, 2015, 2016]
 def opt_bnd(data, k, years):
     available_data = data.copy()
+    optimal_bundle_indices = list()
+    optimal_bundle_value = 0
     for _ in range(k):
+        best_sigma = list()
+        best_sigma_value = float('inf')
         for permutation in permutations(CARS, 5):
             sigma = {p: years[indx] for indx, p in enumerate(permutation)}
-            for indx, (car, year) in enumerate(sigma.items()):
-                car_year_df = pd.DataFrame(available_data.loc[(available_data['brand']==car) & (available_data['year']==year)].id)
-                if indx == 0:
-                    df = car_year_df.copy()
-                else:
-                    df = df.merge(car_year_df, how='cross')
-            pass
+            df = pd.DataFrame()
+            for car, year in sigma.items():
+                car_year_df = available_data.loc[(available_data['brand']==car) & (available_data['year']==year)]
+                df = df.append(car_year_df)
+            S_sigma = permutations(df.index, 5)
+
+            best_bundle_indices = []
+            best_bundle_value = float('inf')
+            for bundle_indices in S_sigma:
+                bundle = available_data.loc[bundle_indices, :]
+                if len(bundle.brand.unique()) == 5:
+                    bundle_value = bundle['value'].sum()
+                    if bundle_value < best_bundle_value:
+                        best_bundle_value = bundle_value
+                        best_bundle_indices = bundle_indices
+
+            sigma_value = best_bundle_value
+
+            if sigma_value < best_sigma_value:
+                best_sigma = best_bundle_indices
+                best_sigma_value = sigma_value
+
+
+        available_data.drop(best_sigma)
+        optimal_bundle_indices.extend(best_sigma)
+        optimal_bundle_value+=best_sigma_value
+        pass
 
 
     # returns the optimal bundle of cars for that k and list of years and their total cost.
-    return {"cost": 0, "bundle": []}
+    return {"cost": optimal_bundle_value, "bundle": optimal_bundle_indices}
 
 
 def comb_vcg(data, k, years):
